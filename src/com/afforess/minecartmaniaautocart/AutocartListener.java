ec89e29d58e5fc61a8cb1d5e7b70493c79934660
afforess <afforess@gmail.com>
2011-04-07 21:35:24 -0400

    Localization!

--- src/com/afforess/minecartmaniaautocart/AutocartListener.java
+++ src/com/afforess/minecartmaniaautocart/AutocartListener.java
@@ -3,6 +3,7 @@ package com.afforess.minecartmaniaautocart;
 import java.util.Calendar;
 
 import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.event.vehicle.VehicleListener;
@@ -10,65 +11,69 @@ import org.bukkit.event.vehicle.VehicleMoveEvent;
 
 import com.afforess.minecartmaniacore.MinecartManiaMinecart;
 import com.afforess.minecartmaniacore.MinecartManiaWorld;
+import com.afforess.minecartmaniacore.config.LocaleParser;
 
 public class AutocartListener extends VehicleListener{
 	
-    public void onVehicleMove(VehicleMoveEvent event) {
-    	if (event.getVehicle() instanceof Minecart) {
-    		MinecartManiaMinecart minecart = MinecartManiaWorld.getMinecartManiaMinecart((Minecart)event.getVehicle());
-    		
-    		if (!minecart.isMoving()) {
-    			//Cooldown to prevent minecarts from running away
-	    		if (Autocart.doCooldown(minecart)) {
-	    			return;
-	    		}
-    		}
-    		
+	public void onVehicleMove(VehicleMoveEvent event) {
+		if (event.getVehicle() instanceof Minecart) {
+			MinecartManiaMinecart minecart = MinecartManiaWorld.getMinecartManiaMinecart((Minecart)event.getVehicle());
+			
+			if (!minecart.isMoving()) {
+				//Cooldown to prevent minecarts from running away
+				if (Autocart.doCooldown(minecart)) {
+					return;
+				}
+			}
+			
 			if (minecart.isOnRails() && (!minecart.isAtIntersection() || !minecart.hasPlayerPassenger())) {
 				if (!MinecartManiaAutocart.isAutocartOnlyForPlayers() || minecart.hasPlayerPassenger()) {
-		    		int l = MinecartManiaWorld.getBlockData(minecart.minecart.getWorld(), minecart.getX(), minecart.getY(), minecart.getZ());
+					int l = MinecartManiaWorld.getBlockData(minecart.minecart.getWorld(), minecart.getX(), minecart.getY(), minecart.getZ());
 	
-			    	int ai[][] = Autocart.trackMetadata[l];
-		            double d8 = ai[1][0] - ai[0][0];
-		            double d10 = ai[1][2] - ai[0][2];
-		            double d12 = minecart.getMotionX() * d8 + minecart.getMotionZ() * d10;
-		            if(d12 < 0.0D)
-		            {
-		                d8 = -d8;
-		                d10 = -d10;
-		            }
-		            
-		            d8 /= 4;
-		            d10 /= 4;
+					int ai[][] = Autocart.trackMetadata[l];
+					double d8 = ai[1][0] - ai[0][0];
+					double d10 = ai[1][2] - ai[0][2];
+					double d12 = minecart.getMotionX() * d8 + minecart.getMotionZ() * d10;
+					if(d12 < 0.0D)
+					{
+						d8 = -d8;
+						d10 = -d10;
+					}
+					
+					d8 /= 4;
+					d10 /= 4;
 	
-		            double d13 = Math.sqrt(minecart.getMotionX() * minecart.getMotionX() + minecart.getMotionZ() * minecart.getMotionZ());
-		            
-		            minecart.setMotionX(d13 > 0.0D ? d8 : 0.0D);
-		            minecart.setMotionZ(d13 > 0.0D ? d10 : 0.0D);
-		            
-		            Autocart.doThrottle(minecart);
+					double d13 = Math.sqrt(minecart.getMotionX() * minecart.getMotionX() + minecart.getMotionZ() * minecart.getMotionZ());
+					
+					minecart.setMotionX(d13 > 0.0D ? d8 : 0.0D);
+					minecart.setMotionZ(d13 > 0.0D ? d10 : 0.0D);
+					
+					Autocart.doThrottle(minecart);
 				}
-	    	}
-    	}
-    }
-    
-    public void onVehicleEnter(VehicleEnterEvent event) {
-    	if (event.isCancelled()) {
-    		return;
-    	}
-    	if (event.getVehicle() instanceof Minecart) {
-    		MinecartManiaMinecart minecart = MinecartManiaWorld.getMinecartManiaMinecart((Minecart)event.getVehicle());
-    		minecart.setDataValue("Cooldown", null);
-    	}
-    }
-    
-    public void onVehicleExit(VehicleExitEvent event) {
-    	if (event.isCancelled()) {
-    		return;
-    	}
-    	if (event.getVehicle() instanceof Minecart) {
-    		MinecartManiaMinecart minecart = MinecartManiaWorld.getMinecartManiaMinecart((Minecart)event.getVehicle());
-    		minecart.setDataValue("Cooldown", new Long(Calendar.getInstance().getTimeInMillis() + 3000));
-    	}
-    }
+			}
+		}
+	}
+	
+	public void onVehicleEnter(VehicleEnterEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+		if (event.getVehicle() instanceof Minecart && event.getVehicle().getPassenger() == null) {
+			MinecartManiaMinecart minecart = MinecartManiaWorld.getMinecartManiaMinecart((Minecart)event.getVehicle());
+			minecart.setDataValue("Cooldown", null);
+			if (event.getEntered() instanceof Player) {
+				((Player)event.getEntered()).sendMessage(LocaleParser.getTextKey("AutoCartEnteredMessage"));
+			}
+		}
+	}
+	
+	public void onVehicleExit(VehicleExitEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+		if (event.getVehicle() instanceof Minecart) {
+			MinecartManiaMinecart minecart = MinecartManiaWorld.getMinecartManiaMinecart((Minecart)event.getVehicle());
+			minecart.setDataValue("Cooldown", new Long(Calendar.getInstance().getTimeInMillis() + 3000));
+		}
+	}
 }
